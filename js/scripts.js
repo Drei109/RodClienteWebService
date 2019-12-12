7a9468a5144c63dc7b3a107a63d6081021651da9
Drei Aguirre <drei.rar@gmail.com>
2019-12-12 17:16:49 -0500

    added categorias

--- js/scripts.js
+++ js/scripts.js
@@ -87,7 +87,7 @@ function ListarProductos(tipo, valor) {
     });
 }
 
-function ListarCategorias() {
+function ListarCategorias(tipo, valor) {
     let productosList = document.getElementById("categoriasList");
     productosList.innerHTML = "";
 
@@ -117,55 +117,31 @@ function ListarCategorias() {
         data: obj,
         url: direccionWebServiceMetodos + urlListar,
         success: function (resp) {
-            let productos = resp.getElementsByTagName("ProductoDTO");
+            let productos = resp.getElementsByTagName("CategoriaDTO");
             for (var i = 0; i < productos.length; i++) {
                 let producto = productos[i];
 
                 var cardDiv = document.createElement("div");
 
-                let imagen = producto.getElementsByTagName("Imagen");
-                let imagenText = imagen[0].textContent;
-                let urlImagen = `${direccionWebService}Uploads/${imagenText}`;
+
+                let categoria = producto.getElementsByTagName("CategoriaId");
+                let categoriaText = categoria[0].textContent;
+                let nombre = producto.getElementsByTagName("Nombre");
+                let nombreText = nombre[0].textContent;
 
                 cardDiv.innerHTML = `<div class="mdl-grid">
                                         <div class="mdl-layout-spacer"></div>
-                                        <div class="mdl-cell mdl-cell--8-col demo-card-wide mdl-card mdl-shadow--2dp">
-                                            <div class="mdl-card__title" style="height: 176px;
-                                            background: url('${urlImagen}') center / cover;">
-                                                <!-- <h2 class="mdl-card__title-text">Welcome</h2> -->
+                                        <div class="mdl-cell mdl-cell--8-col bg-white mdl-shadow--2dp">
+                                            <div class="mdl-card__title">
+                                                <h2 class="mdl-card__title-text">${nombreText}</h2>
+                                            </div>
+                                            <div class="mdl-card__supporting-text">
+                                                ID : ${categoriaText}
                                             </div>                                            
                                         </div>
-                                        <div class="mdl-layout-spacer"></div>
                                     </div>`
 
                 productosList.appendChild(cardDiv);
-
-                var cardSuportingText = document.createElement("div");
-                cardSuportingText.classList.add("mdl-card__supporting-text");
-
-
-                let nodes = producto.getElementsByTagName("*");
-                for (var j = 0; j < nodes.length; j++) {
-                    var li = document.createElement("li");
-                    li.innerHTML = nodes[j].nodeName + ":" + nodes[j].textContent;
-
-                    let divContent = document.createElement("div");
-                    divContent.innerHTML =
-                        `
-                            <div class="mdl-grid margin-b-x">
-                                <div class="mdl-cell mdl-cell--2-col-phone">
-                                    ${nodes[j].nodeName} :
-                                </div>
-                                <div class="mdl-cell mdl-cell--2-col-phone">
-                                    ${nodes[j].textContent}
-                                </div>
-                            </div>
-                        `;
-                    cardSuportingText.appendChild(divContent);
-                }
-
-                let divaux = cardDiv.getElementsByTagName('div')[2];
-                divaux.appendChild(cardSuportingText);
             }
         },
         error: function () {}
@@ -174,7 +150,7 @@ function ListarCategorias() {
 
 
 ListarProductos();
-//ListarCategorias();
+ListarCategorias();
 
 var searchProductos = document.getElementById("searchProductos");
 searchProductos.addEventListener("keyup", function (event) {
@@ -182,7 +158,16 @@ searchProductos.addEventListener("keyup", function (event) {
     var valor = searchProductos.value;
     if (event.keyCode === 13) {
         event.preventDefault();
-        console.log("!");
         ListarProductos(tipo, valor);
+    }
+});
+
+var searchCategorias = document.getElementById("searchCategorias");
+searchCategorias.addEventListener("keyup", function (event) {
+    var tipo = document.querySelector('input[name="opcionCategorias"]:checked').value;
+    var valor = searchCategorias.value;
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        ListarCategorias(tipo, valor);
     }
 });
